0ee099eac98c1da31533f101c880dee96d6cc4f6
leoLUIGY <luigyleonidas@gmail.com>
2026-09-20 23:58:42 -0300

    corrigir rota do swagger ao abrir o projeto

--- obj/Debug/net10.0/music-api-gateway.AssemblyInfo.cs
+++ obj/Debug/net10.0/music-api-gateway.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("music-api-gateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ece23c03146fc9cd1618f15986dc15ac5e2dd7f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a9824a031c83aab30e8eaa069c1e64254cf6a58")]
 [assembly: System.Reflection.AssemblyProductAttribute("music-api-gateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("music-api-gateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
